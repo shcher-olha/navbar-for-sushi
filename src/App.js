211aa74252ad1df8e964e68abb3382fa97faac40
shcher-olha <shcher.olha@gmail.com>
2024-04-05 19:02:27 +0200

    import -router dom+bootstrap+created folder-Components and Pages

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+
+      <button type="button" className="btn btn-primary">
+        Primary
+      </button>
+    </div>
+  );
+}
